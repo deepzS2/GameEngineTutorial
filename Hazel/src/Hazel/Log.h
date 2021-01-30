f61a263718bf685c78c68970e37a3272262550ac
deepzS2 <alanbackman10@hotmail.com>
2021-01-30 09:06:57 -0300

    Adding precompiled headers

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <memory>
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
 #include "Core.h"
@@ -33,8 +32,8 @@ namespace Hazel {
 #define HZ_CORE_FATAL(...)     ::Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 // Client log macros
-#define HZ_ERROR(...)   ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
-#define HZ_WARN(...)    ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define HZ_INFO(...)    ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
-#define HZ_TRACE(...)   ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define HZ_FATAL(...)   ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define HZ_ERROR(...)		   ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HZ_WARN(...)		   ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HZ_INFO(...)		   ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HZ_TRACE(...)		   ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_FATAL(...)		   ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
